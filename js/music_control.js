9e8ce0d6a6b78884d28ba287dc54a0501f8c7172
whisker-studio <1220544903@qq.com>
2020-04-05 14:57:14 +0800

    beta(3)

--- js/music_control.js
+++ js/music_control.js
@@ -3,7 +3,9 @@ const audio = document.querySelector("audio");
 const cp = document.querySelector("#current-point");
 const pb = document.querySelector("#passed-bar");
 const loop = document.querySelector("#loop");
+const vol = document.querySelector("#volumn-control-bar");
 let songList;
+audio.volume = 0.75;
 pause.addEventListener("click", () => {
 	if (audio.paused) {
 		audio.play();
@@ -14,12 +16,14 @@ pause.addEventListener("click", () => {
 	}
 });
 document.querySelector("#next").addEventListener("click", () => {
-	songList.currentOrder++;
+	songList.currentOrder = (songList.currentOrder + 1) % songList.songsLength;
 	songList.loadMusic();
+	audio.play()
 });
 document.querySelector("#pre").addEventListener("click", () => {
-	songList.currentOrder--;
+	songList.currentOrder = songList.currentOrder == 0 ? songList.songsLength - 1 : songList.currentOrder - 1;
 	songList.loadMusic();
+	audio.play()
 });
 loop.addEventListener("click", () => {
 	const img = ["loop", "retweet"]
@@ -27,10 +31,29 @@ loop.addEventListener("click", () => {
 	audio.loop = !audio.loop;
 });
 audio.addEventListener("ended", () => {
-	pause.style.backgroundImage = "url(./images/play.svg)";
+	// pause.style.backgroundImage = "url(./images/play.svg)";
+	songList.currentOrder = (songList.currentOrder + 1) % songList.songsLength;
+	songList.loadMusic();
+	audio.play();
 });
+
+(function () {
+	let volMemory = 0;
+	vol.parentElement.addEventListener("click", function (e) {
+		if (e.target !== this) return;
+		[volMemory, vol.value] = [vol.value, volMemory];
+		updateVol();
+	});
+	vol.addEventListener("change", updateVol);
+	vol.addEventListener("mousemove", updateVol);
+
+	function updateVol() {
+		audio.volume = vol.value / 100;
+		vol.parentElement.style.backgroundImage = `url(./images/vol_${Math.ceil(vol.value/25)}.svg)`;
+	}
+})();
 // 控制进度条的闭包
-(function() {
+(function () {
 	cp.style.left = "0px"; // 初始化对左值
 	let down = false; // 是否在小圈圈内按下鼠标
 	let lastX = cp.getBoundingClientRect().left; // 上一个小圈圈所在的x坐标
@@ -44,7 +67,19 @@ audio.addEventListener("ended", () => {
 		cp.classList.remove("active");
 	});
 	// 如果移动了鼠标
-	document.addEventListener("mousemove", (e) => {
+	document.addEventListener("mousemove", changeProgress);
+	// 小圈圈随时间前进
+	audio.addEventListener("timeupdate", function () {
+		cp.style.left = (this.currentTime / this.duration * 100) + "%"; // 修改样式
+		pb.style.width = (this.currentTime / this.duration * 100) + "%"; // 修改样式
+		lastX = cp.getBoundingClientRect().left; // 记录坐标
+	});
+	cp.parentElement.addEventListener("click",(e)=>{
+		down = true; // 按下鼠标记录
+		changeProgress(e);
+		down = false;
+	});
+	function changeProgress(e){
 		if (!down) return; // 未按下鼠标,返回
 		let left = (parseFloat(cp.style.left) * barLength / 100 + (e.clientX - lastX - cp.clientWidth / 2)) / barLength *
 			100; // 小圈圈对左的百分比
@@ -54,25 +89,18 @@ audio.addEventListener("ended", () => {
 		audio.currentTime = left * audio.duration / 100; // 将当前播放位置跳到对应位置
 		lastX = cp.getBoundingClientRect().left; // 记录坐标
 		e.preventDefault(); // 避免默认行为
-	});
-	// 小圈圈随时间前进
-	audio.addEventListener("timeupdate", function() {
-		cp.style.left = (this.currentTime / this.duration * 100) + "%"; // 修改样式
-		pb.style.width = (this.currentTime / this.duration * 100) + "%"; // 修改样式
-		lastX = cp.getBoundingClientRect().left; // 记录坐标
-	});
+	}
 })();
 
 async function getMusicInfo() {
 	const response = await fetch("./files/music.json");
 	const songs = await response.json();
-	console.log(songs);
+	// console.log(songs);
 	const obj = {
 		currentOrder: 0,
+		songsLength: songs.length,
 		loadMusic() {
-			// audio.src = songs[order].src
-			// audio.src = `https://whisker_studio.gitee.io/${songs[order].src}`
-			audio.src = `http://127.0.0.1/whisker_studio/${songs[this.currentOrder].src}`;
+			audio.src = `./music/songs/${songs[this.currentOrder].src}`
 			document.querySelector("#cover").src = songs[this.currentOrder].cover;
 			document.querySelector("#info").textContent = songs[this.currentOrder].name;
 		}
